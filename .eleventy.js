2003a45af1f4401605baff8ae84b1fc0e60d71d9
Christopher Hannah <me@chrishannah.me>
2024-04-06 16:33:27 +0100

    static pages

--- .eleventy.js
+++ .eleventy.js
@@ -8,60 +8,61 @@ const inspect = require("util").inspect;
 
 module.exports = function (eleventyConfig) {
 
-  const markdownIt = require("markdown-it")
-  const markdownItFootnote = require("markdown-it-footnote")
+    const markdownIt = require("markdown-it")
+    const markdownItFootnote = require("markdown-it-footnote")
 
-  const options = {
-    html: true,
-    breaks: false,
-    linkify: false
-  };
+    const options = {
+        html: true,
+        breaks: false,
+        linkify: false
+    };
 
-  let md = markdownIt(options).use(markdownItFootnote)
-  eleventyConfig.setLibrary('md', md);
+    let md = markdownIt(options).use(markdownItFootnote)
+    eleventyConfig.setLibrary('md', md);
 
-  ['src/assets', 'src/files'].forEach(path => {
-    eleventyConfig.addPassthroughCopy(path, {
-      filter: path => !path.endsWith('.css') && !path.startsWith('_')
-    })
-  })
+    /* passthrough copy static folders */
+    eleventyConfig.addPassthroughCopy({ "static/text-shot": "text-shot" });
+    eleventyConfig.addPassthroughCopy({ "static/2020": "2020" });
+    eleventyConfig.addPassthroughCopy({ "static/cv": "2020" });
+    eleventyConfig.addPassthroughCopy({ "static/prntsc": "prntsc" });
+    eleventyConfig.addPassthroughCopy({ "static/fonts": "fonts" });
 
-  /* Collections */
-  eleventyConfig.addCollection("all", function (collectionApi) {
-    return collectionApi.getAllSorted().filter(function (item) {
-      // Only return content that was originally a markdown file
-      let extension = item.inputPath.split('.').pop();
-      return extension === "md";
+    /* Collections */
+    eleventyConfig.addCollection("all", function (collectionApi) {
+        return collectionApi.getAllSorted().filter(function (item) {
+            // Only return content that was originally a markdown file
+            let extension = item.inputPath.split('.').pop();
+            return extension === "md";
+        });
     });
-  });
 
-  /* Blog Tools */
-  module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(blogTools);
-  };
+    /* Blog Tools */
+    module.exports = function (eleventyConfig) {
+        eleventyConfig.addPlugin(blogTools);
+    };
 
-  /* Filters */
-  eleventyConfig.addFilter("debug", (content) => `${inspect(content)}`);
+    /* Filters */
+    eleventyConfig.addFilter("debug", (content) => `${inspect(content)}`);
 
-  Object.keys(dateFilters).forEach(filterName => {
-    eleventyConfig.addFilter(filterName, dateFilters[filterName])
-  })
+    Object.keys(dateFilters).forEach(filterName => {
+        eleventyConfig.addFilter(filterName, dateFilters[filterName])
+    })
 
-  Object.keys(numberFilters).forEach(filterName => {
-    eleventyConfig.addFilter(filterName, numberFilters[filterName])
-  })
+    Object.keys(numberFilters).forEach(filterName => {
+        eleventyConfig.addFilter(filterName, numberFilters[filterName])
+    })
 
-  Object.keys(postFilters).forEach(filterName => {
-    eleventyConfig.addFilter(filterName, postFilters[filterName])
-  })
+    Object.keys(postFilters).forEach(filterName => {
+        eleventyConfig.addFilter(filterName, postFilters[filterName])
+    })
 
-  /* Watch for changes */
-  eleventyConfig.addWatchTarget("./assets/css/");
+    /* Watch for changes */
+    eleventyConfig.addWatchTarget("./assets/css/");
 
-  return {
-    passthroughFileCopy: true,
-    dir: {
-      output: 'public'
+    return {
+        passthroughFileCopy: true,
+        dir: {
+            output: 'public'
+        }
     }
-  }
 }
